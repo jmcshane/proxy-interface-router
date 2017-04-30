75796e927020ab50158f9e2e8cbe19fe272241de
jmcshane <jmcshan1@gmail.com>
2017-04-29 22:52:30 -0500

    Making the application runnable again

--- src/main/java/com/proxytest/ProxyInterfaceApplication.java
+++ src/main/java/com/proxytest/ProxyInterfaceApplication.java
@@ -1,5 +1,6 @@
 package com.proxytest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,16 @@ import org.springframework.util.ClassUtils;
 @SpringBootApplication
 public class ProxyInterfaceApplication {
 
+	@Autowired
+	IntegerMethodInvocationGenerator generator;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyInterfaceApplication.class, args);
 	}
 
 	@Bean("proxyRouterName")
 	public ProxyRouterBean routerBean() {
-		ProxyRouterBean bean = new ProxyRouterBean(null);
+		ProxyRouterBean bean = new ProxyRouterBean(generator);
 		bean.setBeanClassLoader(ClassUtils.getDefaultClassLoader());
 		bean.setBusinessInterface(IProxiedService.class);
 		return bean;
